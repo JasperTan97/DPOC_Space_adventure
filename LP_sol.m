2dbcfd7748f48d2bbdf902f6faca72547e1ebafb
JasperTan97 <jastan@student.ethz.ch>
2022-11-16 21:48:09 +0100

    LP done

--- LP_sol.m
+++ LP_sol.m
@@ -1,4 +1,4 @@
-function [ J_opt, u_opt_ind ] = Solution(P, G)
+function [ J_opt, u_opt_ind ] = LP_sol(P, G)
 %SOLUTION 
 %   Solve a stochastic shortest path problem by either Value Iteration,
 %   Policy Iteration, or Linear Programming.
@@ -38,5 +38,39 @@ function [ J_opt, u_opt_ind ] = Solution(P, G)
     global TERMINAL_STATE_INDEX
 
     % Do yo need to do something with the teminal state before solving the problem?
+    f = -1 * ones(K,1);
 
+    A_south = eye(K) - P(:,:,SOUTH);
+    A_north = eye(K) - P(:,:,NORTH);
+    A_east = eye(K) - P(:,:,EAST);
+    A_west = eye(K) - P(:,:,WEST);
+    A_stay = eye(K) - P(:,:,STAY);
+
+    A = [A_south; A_north; A_east; A_west; A_stay];
+
+    b_south = G(K, SOUTH);
+    b_north = G(K, NORTH);
+    b_east = G(K, EAST);
+    b_west = G(K, WEST);
+    b_stay = G(K, STAY);
+
+    b = [b_south; b_north; b_east; b_west; b_stay];
+
+    J_opt = linprog(f,A,b);
+    u_opt_ind = NaN(K,1);
+    
+    for state_i = 1:K
+        cost_per_action = zeros(5,1);
+        for action = 1:L
+            % find Q(i,u) + sum[P(i,u,j)*V(j)]
+            state_js = find(P(state_i,:,action)); % look for all nonzero values of state j
+            expected_value = 0; % sum part
+            for state_j = 1:length(state_js) % for each index that are non zeroes
+                expected_value = expected_value + P(state_i, state_j, action) * J_opt(state_j);
+            end
+            cost_per_action(action) = G(state_i, action) + expected_value;
+        end
+        [~, min_idx] = min(cost_per_action);
+        u_opt_ind(state_i) = min_idx;
+    end
 end
